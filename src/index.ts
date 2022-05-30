faaa61aba377f712f0ee12e4619b7aa526a4c57b
Rémy Roy <remyroy@remyroy.com>
2022-05-30 12:07:20 -0400

    Integration with VSCode and initial bot

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-function hello(compiler: string) {
-    console.log(`Hello from ${compiler}`);
-}
-hello("TypeScript");
+import { config } from 'dotenv';
+config();
+
+import { Client, Intents } from 'discord.js';
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN).then(() => {
+  console.log('Discord login successful!');
+}).catch((error) => {
+  console.log(`Error during Discord login: ${error.message}`);
+});
